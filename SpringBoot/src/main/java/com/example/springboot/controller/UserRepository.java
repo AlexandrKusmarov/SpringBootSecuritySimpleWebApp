d04f59f40089e2f310231a439eecfd6bf133bb5a
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2019-03-06 15:03:45 +0300

    Added role selection: ADMIN,USER.

--- SpringBoot/src/main/java/com/example/springboot/controller/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.controller;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Repository
-@RequestMapping("user")
-public class UserRepository {
-}
